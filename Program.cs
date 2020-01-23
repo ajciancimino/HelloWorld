4530e5a75becdb81675f350c120f0841c1d9f3c5
ayrto <ayrto@DESKTOP-E9B3B4I>
2020-01-23 17:31:43 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display press any key to continue message
+            Console.WriteLine("Press any key to continue...");
+            //Display Hello World message
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
